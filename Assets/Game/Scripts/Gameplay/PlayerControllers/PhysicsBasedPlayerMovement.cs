89d1835e89a95315bfea3a1ae2edac69fdfa66b6
Adriano Mazzucco <onairda97@Gmail.com>
2024-11-11 15:12:42 -0500

    Update movement
    started charge attack, need to modify/finish

--- Assets/Game/Scripts/Gameplay/PlayerControllers/PhysicsBasedPlayerMovement.cs
+++ Assets/Game/Scripts/Gameplay/PlayerControllers/PhysicsBasedPlayerMovement.cs
@@ -7,11 +7,25 @@ public class PhysicsBasedPlayerMovement : MonoBehaviour
 {
     [SerializeField] private InputActionAsset inputActions;
     private InputAction movement;
+    private InputAction chargeAttack;
 
     private Rigidbody playerRB;
+    private bool charging = false;
+    private float chargeAmount = 0;
 
     [SerializeField] private GameObject currentCamera;
-    [SerializeField] private GameObject topOfHead;
+
+
+    private void OnEnable()
+    {
+        chargeAttack.performed += StartCharge;
+        chargeAttack.canceled += ChargeAttack;
+    }
+
+    private void OnDisable()
+    {
+        chargeAttack.performed -= StartCharge;
+    }
 
     private void Awake()
     {
@@ -20,23 +34,29 @@ public class PhysicsBasedPlayerMovement : MonoBehaviour
         var playerActionMap = inputActions.FindActionMap("Player");
 
         movement = playerActionMap?.FindAction("Move");
+        chargeAttack = playerActionMap?.FindAction("Fire");
     }
 
     private void Update()
     {
         Movement();
+        ChargeUpdate();
     }
 
     private void Movement()
     {
+        if (charging) return;
+
         Vector2 inputDirection = movement.ReadValue<Vector2>();
         if (inputDirection != Vector2.zero)
         {
+            playerRB.freezeRotation = true;
+
             Vector3 camForward = transform.position - currentCamera.transform.position;
             camForward.y = 0;
             camForward.Normalize();
-            
-            Vector3 camRight = Vector3.Cross(new Vector3(0,1,0), camForward);
+
+            Vector3 camRight = Vector3.Cross(new Vector3(0, 1, 0), camForward);
 
             Vector3 directionTotal = camForward * inputDirection.y + camRight * inputDirection.x;
             directionTotal.Normalize();
@@ -44,11 +64,35 @@ public class PhysicsBasedPlayerMovement : MonoBehaviour
 
             playerRB.AddForce(directionTotal);
 
-            //topOfHead.transform.rotation = Quaternion.LookRotation(directionTotal);
-
             Vector3 turnAngle = new Vector3(directionTotal.x, directionTotal.z, directionTotal.y);
             Quaternion deltaRotation = Quaternion.Euler(turnAngle * Time.fixedDeltaTime);
-            topOfHead.transform.forward = Vector3.Lerp(topOfHead.transform.forward, directionTotal, 0.001f);
+            transform.forward = Vector3.Lerp(transform.forward, directionTotal, 0.005f);
         }
+    }
+
+    private void ChargeUpdate()
+    {
+        if (!charging) return;
+
+        if(chargeAmount < 1)
+            chargeAmount += Time.deltaTime;
+
+        Vector3 euler = transform.localEulerAngles;
+        euler.x = Mathf.Lerp(0, -45, chargeAmount);
+        transform.localEulerAngles = euler;
+    }
+
+    private void StartCharge(InputAction.CallbackContext obj)
+    {
+        charging = true;
+    }
+
+    private void ChargeAttack(InputAction.CallbackContext obj) 
+    {
+        charging = false;
+        chargeAmount = 0;
+        playerRB.freezeRotation = false;
+        // playerRB.AddForce(transform.forward * 100, ForceMode.Impulse);
+        playerRB.AddForceAtPosition(transform.forward * 50, transform.position + transform.up * 1.5f, ForceMode.Impulse);
     }
 }
